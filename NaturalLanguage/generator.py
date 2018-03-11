3772a659316c086d89055b5a811b70ea1c772563
INFJakZda <zdanowski78@gmail.com>
2018-03-11 14:33:16 +0100

    Add weight string generator - first row (1.3)

--- NaturalLanguage/generator.py
+++ NaturalLanguage/generator.py
@@ -30,17 +30,37 @@ def mean_length(string):
 #counts number of occurrences of every letter in given string
 def lett_counter(string, dictionary):
     letters = collections.Counter(string)
-    length = len(string)
-
+    length = 0
+    prop_list = []
     for lett in dictionary:
-        print(lett, letters[lett] / length )
+        length += letters[lett]
+        temp = letters[lett]
+        prop_list.append(temp)
+        
+    for i in range(0, len(prop_list)):
+        prop_list[i] = prop_list[i] / length
+    return prop_list
+
+#generates random string from alphabet & letter_propability list
+#The approximation of the first row
+def propability_generator(dictionary, prop_list):
+    string = ""
+    diction = []
+    for lett in dictionary:
+        diction.append(lett)
+    string = numpy.random.choice(diction, length, p = prop_list)
+    print(''.join(string))
+    return string
     
 
 if __name__ == '__main__':
-    dictionary = "abcdefghijklmnoprstuwxyz "
-    length = 10000000
+    dictionary = "abcdefghijklmnoprstuwxyz 0123456789"
+    length = 100000
     f = open("data/norm_wiki_sample.txt","r")
     string = f.read()
-    #string = generator(dictionary)
+    #string = generator(dictionary, length)
     #mean_length(string)
-    lett_counter(string, dictionary)
+    prob_list = lett_counter(string, dictionary)
+    string = propability_generator(dictionary, prob_list)
+    mean_length(string)
+    
