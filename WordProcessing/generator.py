f0b25dc9d40f5d7d9d258edca6a4d314f19e9be8
INFJakZda <zdanowski78@gmail.com>
2018-03-20 23:25:15 +0100

    Add random words generator (2.2)

--- WordProcessing/generator.py
+++ WordProcessing/generator.py
@@ -2,34 +2,45 @@ import random
 import numpy
 import collections
 
-def words_counter(text):
-    #seperate words
-    words = text.split(" ")
-    
-    #delete whitespace , new line, tabs
-    words = [w for w in words if (len(w) > 0 and w != '\n' and w != '\t')]
-    
-    counter = collections.Counter(words)
 
+def text_normaliser(text):
+    #seperate words
+    words = text.split(" ")    
+    #delete whitespace , new line, tabs
+    words = [w for w in words if (len(w) > 0 and w != '\n' and w != '\t')]    
+    return words
+
+def words_counter(words):    
+    counter = collections.Counter(words)
     #print ascending
     for word in counter.most_common(100):
-        print(word) 
-    
+        print(word)     
     #print random order    
     #for word in counter:
     #    print(word, counter[word])
+
+#random words generator
+def words_generator(text, length):
+    rand_text = ""
+    len_text = len(text) - 1
+    for i in range(length):
+	    rand_text += text[random.randint(0, len_text)] + ' '
+    return rand_text
 
 def read_file(filename):
     f = open(filename, "r")
     return f.read()
 
 if __name__ == '__main__':
-    
     text = read_file("data/norm_wiki_sample.txt")
-    #zad1
-    words_counter(text)
+    words = text_normaliser(text)
     
+    #zad1
+    #words_counter(words)
+       
     #zad2
+    rand_text = words_generator(words, 100)
+    print(rand_text)
     
     #zad3
     
