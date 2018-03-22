0656fb8dfaaaf87e2c98bc620b354a0704c2ae40
INFJakZda <zdanowski78@gmail.com>
2018-03-22 15:38:17 +0100

    Add print [most pop / all] in (2.1)

--- WordProcessing/generator.py
+++ WordProcessing/generator.py
@@ -10,11 +10,21 @@ def text_normaliser(text):
     words = [w for w in words if (len(w) > 0 and w != '\n' and w != '\t')]    
     return words
 
-def words_counter(words):    
+def words_counter(words, noMostPop, noToPrint):
+    most_popular = 0
+    all_words = len(words)
     counter = collections.Counter(words)
     #print ascending
-    for word in counter.most_common(100):
-        print(word)     
+    for word in counter.most_common(noMostPop):
+        if(noToPrint > 0):
+            print(word, word[1] / all_words)
+            noToPrint -= 1
+        most_popular += word[1]
+    print(most_popular)
+    print(all_words)
+    print(most_popular / all_words)
+    
+             
     #print random order    
     #for word in counter:
     #    print(word, counter[word])
@@ -36,11 +46,11 @@ if __name__ == '__main__':
     words = text_normaliser(text)
     
     #zad1
-    #words_counter(words)
+    words_counter(words, 30000, 30)
        
     #zad2
-    rand_text = words_generator(words, 100)
-    print(rand_text)
+    #rand_text = words_generator(words, 100)
+    #print(rand_text)
     
     #zad3
     
