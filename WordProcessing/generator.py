48cbf876f5e68f8ec8b59e43fd3f866de513549f
INFJakZda <zdanowski78@gmail.com>
2018-03-25 20:06:11 +0200

    First version of markov chain gen

--- WordProcessing/generator.py
+++ WordProcessing/generator.py
@@ -61,18 +61,50 @@ def words_propab_gen(counter, cnt_len, length):
     print(gen_words)
 
 
+def getWordAfter(words, word):
+    list_words = []
+    len_word = len(word)
+    tmp_list = []
+    for i in range(0, len_word):
+        tmp_list.append(words[i])
+    for ele in range(len_word, len(words)):
+        if(tmp_list == word):
+            list_words.append(words[ele].rstrip())
+        tmp_list.pop(0)
+        tmp_list.append(words[ele])
+    return list_words
+        
+
+
+def markov_chain(words, level, length):
+    outText = []
+    outText.append(words[random.randint(0, len(words) - 1)])
+    for i in range(0, length):
+        #print(outText)
+        word_list = getWordAfter(words, outText[-level:])
+        tmp_level = level
+        while( len(word_list) == 0 and tmp_level > 1):
+            print("ni ma szukamy: " + str(tmp_level))
+            tmp_level -= 1
+            word_list = getWordAfter(words, outText[-tmp_level:])
+        outText.append(word_list[random.randint(0,len(word_list)-1)])
+    print(outText)
+    
+    
 if __name__ == '__main__':
-    #text = read_file("data/norm_wiki_sample.txt")
-    text = read_file("text.txt")
+    text = read_file("data/norm_wiki_sample.txt")
+    #text = read_file("text.txt")
     words = text_normaliser(text)
     
     #zad1
-    counter, cnt_len = words_counter(words, 30000, 30)
+    #counter, cnt_len = words_counter(words, 30000, 30)
        
     #zad2.1
     #rand_text = words_generator(words, 100)
     #print(rand_text)
     #zad2.2
-    words_propab_gen(counter, cnt_len, 100)   
+    #words_propab_gen(counter, cnt_len, 100)   
     
-    #zad3
+    #zad3.1
+    markov_chain(words, 2, 10)
+    
